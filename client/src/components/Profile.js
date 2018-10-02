271bea2cb09b88d1844d5c84f39d17209985d186
MrTj458 <mrtj458@gmail.com>
2018-10-02 11:10:10 -0600

    Added dropzone

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Dropzone from 'react-dropzone'
 import {
 	Grid,
 	Divider,
@@ -7,19 +8,90 @@ import {
 	Container,
 	Image,
 	Header,
+	Form,
 } from 'semantic-ui-react'
 
 const defaultImage = 'https://d30y9cdsu7xlg0.cloudfront.net/png/15724-200.png'
 
 class Profile extends React.Component {
-	state = { editing: false }
+	state = {
+		editing: false,
+		formValues: {
+			name: '',
+			email: '',
+			file: '',
+		},
+	}
+
+	static getDerivedStateFromProps(props, state) {
+		const { user } = props
+		const { formValues, editing } = state
+		if(user.name !== formValues.name && !editing) {
+			return { formValues: { name: user.name, email: user.email } }
+		}
+		return null
+	}
+
+	onDrop = files => {
+		this.setState(state => ({
+			formValues: {
+				...state.formValues,
+				file: files[0],
+			}
+		}))
+	}
 	
 	toggleEdit = () => {
 		this.setState( state => ({ editing: !state.editing }))
 	}
 
-	editView = () => {
+	handleSubmit = e => {
+		e.preventDefault()
+	}
 
+	handleChange = e => {
+		const { name, value } = e.target
+		this.setState(state => ({
+			formValues: {
+				...state.formValues,
+				[name]: value,
+			}
+		}))
+	}
+
+	editView = () => {
+		const { formValues: { name, email, file } } = this.state
+
+		return (
+			<Form onSubmit={this.handleSubmit}>
+				<Grid.Column width={4}>
+					<Dropzone
+						onDrop={this.onDrop}
+						multiple={false}
+					>
+						{ file && <Image src={file.preview} alt="upload preview" /> }
+					</Dropzone>
+				</Grid.Column>
+				<Grid.Column eidth={8}>
+					<Form.Input
+						label="Name"
+						name="name"
+						value={name}
+						required
+						onChange={this.handleChange}
+					/>
+					<Form.Input
+						label="Email"
+						name="email"
+						value={email}
+						required
+						onChange={this.handleChange}
+						type="email"
+					/>
+					<Button>Update</Button>
+				</Grid.Column>
+			</Form>
+		)
 	}
 
 	profileView = () => {
