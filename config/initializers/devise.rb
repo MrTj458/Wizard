5812ac3eac86e4a7641cc7ac8f366a99ce38c869
MrTj458 <mrtj458@gmail.com>
2018-10-02 09:54:35 -0600

    Fixed devise secret key

---
+++ config/initializers/devise.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Devise.setup do |config|
+  config.secret_key = '100447cb31cbabe37982108edd600a6cb0c2c570e2c0b2b88a424204e24a7c3668c3628c27c94dfea6c590962e267f8d5b66ab21f1c3c911f824bd50ece93093'
+end
