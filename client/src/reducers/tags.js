cc8f413a71505910fa9d4827ed83bdd17be33e09
MrTj458 <mrtj458@gmail.com>
2018-10-03 11:22:47 -0600

    Finished tags

--- client/src/reducers/tags.js
+++ client/src/reducers/tags.js
@@ -1,6 +1,15 @@
 import axios from 'axios'
 
+const TAGS = 'TAGS'
 const ADD_TAG = 'ADD_TAG'
+const DELETE_TAG = 'DELETE_TAG'
+
+export const getTags = () => {
+	return dispatch => {
+		axios.get('/api/tags')
+			.then( res => dispatch({ type: TAGS, tags: res.data }))
+	}
+}
 
 export const addTag = tag => {
 	return dispatch => {
@@ -12,11 +21,22 @@ export const addTag = tag => {
 	}
 }
 
+export const deleteTag = id => {
+	return dispatch => {
+		axios.delete(`/api/tags/${id}`)
+			.then( res => dispatch({ type: DELETE_TAG, id }))
+	}
+}
+
 
 export default (state = [], action) => {
 	switch(action.type) {
+		case TAGS:
+			return action.tags
 		case ADD_TAG:
 			return [...state, action.tag]
+		case DELETE_TAG:
+			return state.filter( tag => tag.id !== action.id)
 		default:
 			return state
 	}
